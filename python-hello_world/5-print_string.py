66226d7aff495a38f4d860e1a5f3e7690e347f18
kleber89 <benkosisackson@gmail.com>
2024-05-08 22:02:17 -0500

    print 3 times a string stored in the variable

--- python-hello_world/5-print_string.py
+++ python-hello_world/5-print_string.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 
-str = "Holberton School"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-
-str_v = "Holberton School"
-
-print ((str_v + '\n') * 3 + str_v[:9] + '\n')
+str_value = "Holberton School"
+print((str_value + '\n') * 3 + str_value[:9] + '\n')
