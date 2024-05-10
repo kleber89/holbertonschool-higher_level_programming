8f1af14381316c218ba7cd918659a3386ff2ff2b
kleber89 <benkosisackson@gmail.com>
2024-05-10 16:47:49 -0500

    print the integer stored in the variable

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 number = 98
 
-print(f"{number}, Battery Street,\n")  # Using f-string to print the integer followed by "Battery Street" and a new line
+print(f"{number}, Battery Street,\n") 
