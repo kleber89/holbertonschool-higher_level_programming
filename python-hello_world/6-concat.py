43383673724df610dd268b03bb8a88a98511ef3e
kleber89 <benkosisackson@gmail.com>
2024-05-24 16:59:19 -0500

    update print string2

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-str1 = "Welcome to"
-str2 = "Holberton"
-str3 = "School"
-print(f"{str1} {str2} {str3}!")
+str1 = "Holberton"
+str2 = "School"
+str3 = "Welcome to"
+print(f"{str3} {str1} {str2}!")
+
