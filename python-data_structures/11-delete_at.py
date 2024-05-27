82c9a98048531fab826bdd51623a3c83a6b12690
kleber89 <benkosisackson@gmail.com>
2024-05-26 21:16:48 -0500

    deletes the item at a specific position in a list

---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def eliminar_at(my_list=[], idx=0):
+    # Verificar si el índice es negativo o está fuera del rango de la lista
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    # Eliminar el elemento en el índice especificado
+    new_list = my_list[:idx] + my_list[idx + 1:]
+
+    return new_list
