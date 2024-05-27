5bd75b54d4535237e0434d61d461d83e3c489492
kleber89 <benkosisackson@gmail.com>
2024-05-26 21:13:14 -0500

    adds 2 tuples

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    # la tupla a debe tener como minimo 2 elementos
+    if len(tuple_a) < 2:
+        if len(tuple_a) == 1:
+            tuple_a = (tuple_a[0], 0)
+        else:
+            tuple_a = (0, 0)
+    else:
+        tuple_a = tuple_a[:2]
+    # la tupla b también debe contar minimo con 2 elementos    
+    if len(tuple_b) < 2:
+        if len(tuple_b) == 1:
+            tuple_b = (tuple_b[0], 0)
+        else:
+            tuple_b = (0, 0)
+    else:
+        tuple_b = tuple_b[:2]
+    sum_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return sum_tuple
