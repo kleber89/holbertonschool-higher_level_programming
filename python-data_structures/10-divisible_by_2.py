45adbd2e7113c775c536e1decdf0856cd60fe5b6
kleber89 <benkosisackson@gmail.com>
2024-06-01 11:32:34 -0500

    update fix pycodestyle final

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     result_list = []
     for num in my_list:
         if num % 2 == 0:
-            result_list.append(True)  # Si es divisible por 2 sera un True
+            result_list.append(True)
         else:
-            result_list.append(False)  # Si no se puede dividir por 2 sera un False
+            result_list.append(False)
     return result_list
