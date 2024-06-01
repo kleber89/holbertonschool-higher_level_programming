a3eb64afcf10516d70bf70deb8599232d4809e55
kleber89 <benkosisackson@gmail.com>
2024-06-01 11:27:55 -0500

    update fix pycodestyle

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     if not my_list:
         return None
     max_num = my_list[0]
-    for num in my_list[1:]: 
+    for num in my_list[1:]:
         if num > max_num:
             max_num = num
     return max_num
