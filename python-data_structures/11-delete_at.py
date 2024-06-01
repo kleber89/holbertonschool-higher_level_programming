94d86a0af2092349370bc0b23e1603d2bf34b55d
kleber89 <benkosisackson@gmail.com>
2024-06-01 11:40:48 -0500

    update fix code and pycodestyle

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def eliminar_at(my_list=[], idx=0):
+def delete_at(my_list=[], idx=0):
     # Verificar si el índice es negativo o está fuera del rango de la lista
     if idx < 0 or idx >= len(my_list):
         return my_list
