6588e50fec11ee36dc5f2b9877d81dc661fa0fc5
kleber89 <benkosisackson@gmail.com>
2024-06-02 21:22:14 -0500

    fix print number

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-
 for i in range(100):
-    # Print the number with two digits followed by ", " except for the last number
     print("{:02d}".format(i), end=', ' if i < 99 else '\n')
