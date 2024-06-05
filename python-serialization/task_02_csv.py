4009a5bb6a1b25a37f36e362dfe102490b337779
kleber89 <benkosisackson@gmail.com>
2024-06-05 08:13:20 -0500

    converting it into another format (JSON) using serialization

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import csv
+import json
+
+def convert_csv_to_json(csv_filename):
+    
+    try:
+        # Read the CSV file and convert each row into a dictionary
+        with open(csv_filename, mode='r', newline='') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            data = [row for row in csv_reader]
+
+        # Serialize the list of dictionaries to JSON
+        with open('data.json', mode='w') as json_file:
+            json.dump(data, json_file, indent=4)
+
+        return True
+
+    except FileNotFoundError:
+        print(f"File {csv_filename} not found.")
+        return False
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return False
