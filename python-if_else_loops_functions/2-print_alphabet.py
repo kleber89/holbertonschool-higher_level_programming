bc1b2d5f4050f9baa263886f8e981eae60884b4d
kleber89 <benkosisackson@gmail.com>
2024-06-08 10:43:32 -0500

    update print alphabet

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -2,5 +2,4 @@
 
 # Loop through ASCII values of lowercase alphabet (97 to 122)
 for i in range(97, 123):
-    # Print each character without newline using string formatting
     print(chr(i), end='')
