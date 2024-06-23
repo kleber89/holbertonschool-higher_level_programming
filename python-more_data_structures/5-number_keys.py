bf965ad43f542c63f70f54e04b11d7d4e93804c4
kleber89 <benkosisackson@gmail.com>
2024-06-23 13:26:16 -0500

    update code and style

--- python-more_data_structures/5-number_keys.py
+++ python-more_data_structures/5-number_keys.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    # Uso len() para contar el numero de claves en el diccionario 
     num_keys = len(a_dictionary)
     return num_keys
-
-# Ejemplo de uso
-my_dictionary = {'a': 1, 'b': 2, 'c': 3}
-result = number_keys(my_dictionary)
-print(result)
