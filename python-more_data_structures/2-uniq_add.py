aa0bd03cb346c659baaae328ccf20bf930ecb8eb
kleber89 <benkosisackson@gmail.com>
2024-06-23 13:09:28 -0500

    update unique integers

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,18 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    # Uso de conjunto para el seguimiento de los numeros enteros unicos
-    unique_integers = set()
-
-    # Iterateracion de la lista y add de los nnumeros enteros
+    unique_numbers = set()
     for num in my_list:
-        unique_integers.add(num)
-
-    # Suma de todos los numeros enteros
-    sum_unique = sum(unique_integers)
+        unique_numbers.add(num)
+        sum_unique = sum(unique_numbers)
 
     return sum_unique
-
-# Ejemplo de uso
-my_list = [1, 2, 2, 3, 4, 4, 5]
-result = uniq_add(my_list)
-print(result)
