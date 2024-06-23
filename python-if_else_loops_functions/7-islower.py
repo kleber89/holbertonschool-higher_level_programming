319308dd30886babb46dcd77450917b7a343e2ca
kleber89 <benkosisackson@gmail.com>
2024-06-23 11:24:34 -0500

    update code

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
 
 def islower(c):
-
-    # Check if the ASCII value of the character falls within the range of lowercase letters
-    return ord('a') <= ord(c) <= ord('z')
-
-# Test cases
-print(islower('a'))  # True
-print(islower('A'))  # False
-print(islower('z'))  # True
-print(islower('Z'))  # False
-print(islower('1'))  # False
+    return ord(c) >= 97 and ord(c) <= 122
