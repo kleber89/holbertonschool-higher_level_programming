b4847a8844a2b15ab731782108f5a2ace5a3bef4
kleber89 <benkosisackson@gmail.com>
2024-06-23 12:09:37 -0500

    fix code

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
 import sys
 
+
 def main():
-    argv = sys.argv[1:]  # Exclude the script name
-    num_args = len(argv)
+    argv = sys.argv[1:]
+    argc = len(argv)
 
-    if num_args == 0:
-        print("Number of argument(s):.")
-    elif num_args == 1:
-        print(f"Number of argument(s): {num_args} argument:")
+    if argc == 0:
+        print("Number of argument(s): 0.")
     else:
-        print(f"Number of argument(s): {num_args} arguments:")
+        print(
+            "Number of argument(s): {} argument{}:".format(
+                argc, "" if argc == 1 else "s"
+            )
+        )
+        for i, arg in enumerate(argv, start=1):
+            print("{}: {}".format(i, arg))
 
-    for i, arg in enumerate(argv, start=1):
-        print(f"{i}: {arg}")
 
 if __name__ == "__main__":
     main()
