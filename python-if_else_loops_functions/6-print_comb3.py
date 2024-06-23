a171244ad9917bf7b3a5671862afd51c48b22870
kleber89 <benkosisackson@gmail.com>
2024-06-23 11:19:39 -0500

    fix code and style

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i + 1, 10):
-        print("{:d}{:d}".format(i, j), end=', ' if not (i == 8 and j == 9) else '\n')
+        if i == 8 and j == 9:
+            print("{:d}{:d}".format(i, j))
+        else:
+            print("{:d}{:d}".format(i, j), end=', ')
