50d91d52a0e2a2852511ef568d4cde88c6f74388
kleber89 <benkosisackson@gmail.com>
2024-06-23 14:06:14 -0500

    fix code best score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
+
 def best_score(a_dictionary):
-    # Inicializo variables para realizar un seguimiento de la mejor puntuación y su clave correspondiente
-    best_key = None
-    best_score = float('-inf')  # Inicialice con infinito negativo para garantizar que se considere cualquier valor en el diccionario.
+    if a_dictionary is None:
+        return None
 
-    # Itero sobre los elementos del diccionario.
-    for key, value in a_dictionary.items():
-        # Compruebo si el valor actual es mayor que la mejor puntuación actual
-        if value > best_score:
-            best_score = value
-            best_key = key
+    maxScore = 0
+    maxKey = None
 
-    return best_key
+    for k in a_dictionary:
+        if a_dictionary[k] > maxScore:
+            maxScore = a_dictionary[k]
+            maxKey = k
 
-# Ejemplo de uso
-my_dictionary = {'a': 85, 'b': 92, 'c': 78, 'd': 95}
-result = best_score(my_dictionary)
-print(result)
+    return maxKey
