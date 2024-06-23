3b7faf7f789c72e8f59979fb0caf51ae39f8b1d1
kleber89 <benkosisackson@gmail.com>
2024-06-23 13:21:19 -0500

    fix code and style

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    # Calculo la diferencia entre set_1 y set_2
-    diff_set_1 = set_1.symmetric_difference(set_2)
-
-    return diff_set_1
-
-# Ejemplo de uso
-set_1 = {1, 2, 3, 4, 5}
-set_2 = {4, 5, 6, 7, 8}
-
-result = only_diff_elements(set_1, set_2)
-print(result)
+    diff_set = set_1 ^ set_2
+    return diff_set
