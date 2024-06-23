c166649078182aad5aa8528ce23d32f13a58e390
kleber89 <benkosisackson@gmail.com>
2024-06-23 13:55:52 -0500

    fix code multiply by 2

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
-def multiplicar_por_2(a_diccionario):
-    # Creo un nuevo diccionario para almacenar los valores multiplicados por 2
-    nuevo_diccionario = {}
+def multiply_by_2(a_dictionary):
+    result = {}
 
-    # Iteramos sobre los elementos del diccionario original
-    for clave, valor in a_diccionario.items():
-        # Multiplico el valor por 2 y lo asignamos a la clave correspondiente en el nuevo diccionario
-        nuevo_diccionario[clave] = valor * 2
-
-    return nuevo_diccionario
-
-# Ejemplo de uso:
-mi_diccionario = {'a': 1, 'b': 2, 'c': 3}
-resultado = multiplicar_por_2(mi_diccionario)
-print(resultado)
+    for k in a_dictionary:
+        result[k] = a_dictionary[k] * 2
+    return result
