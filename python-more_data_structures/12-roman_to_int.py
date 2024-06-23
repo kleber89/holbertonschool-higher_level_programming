d51dbb2a22395917b94443d3b57d6179af5973bd
kleber89 <benkosisackson@gmail.com>
2024-06-23 14:23:08 -0500

    update code roman to Integer

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or roman_string is None:
+    if not isinstance(roman_string, str) or not roman_string:
         return 0
 
-    roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    result = 0
+    roman_numerals = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+
+    total = 0
     prev_value = 0
 
-    for char in reversed(roman_string):
-        value = roman_numerals.get(char, 0)
-        if value < prev_value:
-            result -= value
-        else:
-            result += value
-        prev_value = value
+    for char in roman_string:
+        current_value = roman_numerals.get(char, 0)
+        total += current_value
 
-    return result
+        if current_value > prev_value:
+            total -= 2 * prev_value
 
-# Ejemplo de uso:
-numero_romano = "MCMLIV"
-resultado = roman_to_int(numero_romano)
-print(resultado)
+        prev_value = current_value
+
+    return total
