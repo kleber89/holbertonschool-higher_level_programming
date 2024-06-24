31bb7ad020686847c9151a1f176d2bc95bef310c
kleber89 <benkosisackson@gmail.com>
2024-06-24 15:09:14 -0500

    fix code raise exception

--- python-exceptions/5-raise_exception.py
+++ python-exceptions/5-raise_exception.py
@@ -1,10 +1,4 @@
 #!#!/usr/bin/python3
 def raise_exception():
     # Generamos una excepción de tipo
-    raise TypeError("Esto es una excepción de tipo")
-
-# Ejemplo de uso:
-try:
-    raise_exception()
-except TypeError as e:
-    print("Se produjo un error:", e)
+    raise TypeError("This is raise Exception")
