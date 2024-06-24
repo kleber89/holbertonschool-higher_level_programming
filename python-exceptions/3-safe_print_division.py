7222feaf6f347c28d5b7d28b09029781dba507fe
kleber89 <benkosisackson@gmail.com>
2024-06-24 14:53:06 -0500

    fix code integers division with debug

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,34 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    result = None
     try:
-        # Intentar realizar la división
-        resultado = a / b
+        result = a / b
     except ZeroDivisionError:
-        # Manejar el error de división por cero
-        print("Error: División por cero")
-        return None
-    except TypeError:
-        # Manejar el error si b no es un número
-        print("Error: El divisor no es un número")
-        return None
-    except Exception as e:
-        # Manejar otras excepciones
-        print("Se produjo un error:", e)
-        return None
-    else:
-        # Si no ocurrió ninguna excepción, imprimir el resultado
-        print("Resultado interno: {}".format(resultado))
-        return resultado
+        result = None
     finally:
-        # Este bloque se ejecuta siempre, independientemente de si ocurrió una excepción o no
-        print("Bloque finally ejecutado")
-
-# Ejemplo de uso:
-resultado = safe_print_division(10, 2)
-print("Resultado:", resultado)
-
-resultado = safe_print_division(10, 0)
-print("Resultado:", resultado)
-
-resultado = safe_print_division("10", 2)
-print("Resultado:", resultado)
+        print("Inside result: {}".format(result))
+    return result
