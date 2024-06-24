9ba536bf72f1d7acb11fee3b63f374c20bcd2618
kleber89 <benkosisackson@gmail.com>
2024-06-24 15:15:21 -0500

    fix code and style

--- python-exceptions/6-raise_exception_msg.py
+++ python-exceptions/6-raise_exception_msg.py
@@ -1,10 +1,3 @@
-#!#!/usr/bin/python3
-def raise_exception_msg(mensaje=""):
-    # Genera una excepción de nombre con el mensaje proporcionado
-    raise NameError(mensaje)
-
-# Ejemplo de uso:
-try:
-    raise_exception_msg("Este es un mensaje personalizado para la excepción")
-except NameError as e:
-    print("Se produjo un error:", e)
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    raise NameError(message)
