17d1171d47a229a965ac64a159ad038dab8c3543
kleber89 <benkosisackson@gmail.com>
2024-06-24 15:41:16 -0500

    fix code

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -3,10 +3,16 @@
 
 
 class Square:
+    """Empty class Square that defines a square."""
+
     def __init__(self, size=0):
-        if not isinstance(size, int):
+        """Initializes the data."""
+        self.__size = size
+
+        if not type(self.__size) is int:
+            """Raise an error if size is not an integer."""
             raise TypeError("size must be an integer")
-        elif size < 0:
+
+        if self.__size < 0:
+            """Raise an error if size is less than 0."""
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
