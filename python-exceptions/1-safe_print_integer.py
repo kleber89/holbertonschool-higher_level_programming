9a37c00b1ec0c2e9c4b0805d850345039cebaab0
kleber89 <benkosisackson@gmail.com>
2024-06-24 14:30:33 -0500

    fix code safe printing of an integers list

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,20 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        # Intento formatear e imprimir el valor como un número entero
         print("{:d}".format(value))
         return True
     except (ValueError, TypeError):
-        # Si ocurre ValueError o TypeError, significa que el valor no se puede imprimir como un número entero
         return False
-
-# Ejemplo de uso
-value1 = 42
-value2 = "Hello"
-
-print("Value 1:")
-print("Printed successfully:", safe_print_integer(value1))
-
-print("\nValue 2:")
-print("Printed successfully:", safe_print_integer(value2))
-
