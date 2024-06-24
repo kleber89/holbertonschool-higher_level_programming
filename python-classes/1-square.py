9cd534a7c5047954bfed35d4c5da6ba5cff3660d
kleber89 <benkosisackson@gmail.com>
2024-06-24 15:26:17 -0500

    add comment to the code and fix style

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""Empty class Square that defines a square."""
+
+
 class Square:
+    """Empty class Square that defines a square."""
+
     def __init__(self, size):
         self.__size = size  # Private instance attribute
