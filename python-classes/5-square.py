8323f583a77a7dbb363ea01d18714122632c0064
kleber89 <benkosisackson@gmail.com>
2024-06-25 23:38:32 -0500

    update code

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -5,32 +5,17 @@ class Square:
     """
 
     def __init__(self, size=0):
-        """
-        Initializes the square with an optional size parameter.
-        Args:
-            size (int): The size of the square, default is 0.
-        """
+        """Initializes the square with an optional size parameter."""
         self.size = size  # This will use the setter to validate size
 
     @property
     def size(self):
-        """
-        Property to retrieve the size of the square.
-        Returns:
-            int: The size of the square.
-        """
+        """Property to retrieve the size of the square"""
         return self._size
 
     @size.setter
     def size(self, value):
-        """
-        Property setter to set the size of the square.
-        Args:
-            value (int): The size value to set.
-        Raises:
-            TypeError: If size is not an integer.
-            ValueError: If size is less than 0.
-        """
+        """Property setter to set the size of the square"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -38,18 +23,11 @@ class Square:
         self._size = value
 
     def area(self):
-        """
-        Calculates and returns the area of the square.
-        Returns:
-            int: The area of the square.
-        """
+        """Calculates and returns the area of the square"""
         return self._size ** 2
 
     def my_print(self):
-        """
-        Prints the square using the '#' character.
-        If size is 0, prints an empty line.
-        """
+        """Prints the square using the '#' character"""
         if self._size == 0:
             print()
         else:
