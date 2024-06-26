30c00545eb09ea492427ae67b55e732824271f50
kleber89 <benkosisackson@gmail.com>
2024-06-26 01:52:43 -0500

    load fix code

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 class Rectangle:
-    def __init__(self):
-        pass
+    """
+    This class defines a rectangle.
+
+    Attributes:
+        (None yet)
+
+    Methods:
+        (None yet)
+    """
+    pass
