5ed1aead6fbf2c9db4fe58acf8afb94d5d5b578d
kleber89 <benkosisackson@gmail.com>
2024-06-26 15:40:04 -0500

    update code

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""
+Defines an empty Rectangle class.
+
+This class serves as a blueprint for creating rectangle objects
+with specific attributes and behaviors. However, currently,
+the class definition is empty and doesn't provide any functionality
+for working with rectangles.
+"""
+
+
 class Rectangle:
     """
     This class defines a rectangle.
