c34d3a065c36fdde5ceecdd35d2eb22a82b96b01
kleber89 <benkosisackson@gmail.com>
2024-06-26 23:33:29 -0500

    load, add and save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,17 +1,29 @@
 #!/usr/bin/python3
-"""module that add and save all arguments of a list"""
+"""
+Module that adds and saves all arguments to a list.
+"""
 
 import sys
 from pathlib import Path
-
-
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+from 5-save_to_json_file import save_to_json_file
+from 6-load_from_json_file import load_from_json_file
 
 filename = "add_item.json"
-items = []
 
+"""
+Check if the file exists, and load its content if it does
+"""
 if Path(filename).exists():
     items = load_from_json_file(filename)
+else:
+    items = []
+
+"""
+Extend the existing items list with command-line arguments
+"""
 items.extend(sys.argv[1:])
+
+"""
+Save the updated list back to the JSON file
+"""
 save_to_json_file(items, filename)
