de2b68fea6d047e1e17eeb0c5da9225cb651fd23
kleber89 <benkosisackson@gmail.com>
2024-06-26 23:10:23 -0500

    update to JSON string

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -5,11 +5,6 @@ import json
 
 def to_json_string(my_obj):
     """
-    Converts an object (dictionary or list) into its JSON representation as a string.
-
-    Parameters:
-    my_obj (object): The object to be converted to JSON.
-
     Returns:
     str: The JSON representation of the object as a string.
     """
