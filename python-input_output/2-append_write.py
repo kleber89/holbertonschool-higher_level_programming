3d5d0eb981578b9da083a9b678b4b64ce8aba18f
kleber89 <benkosisackson@gmail.com>
2024-06-26 23:02:55 -0500

    update append to a file

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,17 +3,6 @@
 
 
 def append_write(filename="", text=""):
-    """
-    Appends a string to the end of a UTF-8 encoded text file and returns the number of characters added.
-
-    Parameters:
-    filename (str): The name of the file to append to (default is an empty string).
-    text (str): The string to append to the file (default is an empty string).
-
-    Returns:
-    int: The number of characters added to the file.
-
-    """
-    with open(filename, "a", encoding="utf-8") as file:
-        file.write(text)
-        return len(text)
+    """Appends a string to a text file (UTF8)"""
+    with open(filename, "a") as f:
+        return f.write(text)
