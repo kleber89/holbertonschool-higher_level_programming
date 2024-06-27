1cbc87124ded3446e47d980f7c71398c5fa58d39
kleber89 <benkosisackson@gmail.com>
2024-06-26 23:07:45 -0500

    To JSON string

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
+"""module json"""
+import json
+
+
 def to_json_string(my_obj):
-    """Devuelve la representación JSON de un objeto como una cadena."""
-    if isinstance(mi_obj, str):
-        return '"' + mi_obj.replace('\\', '\\\\').replace('"', '\\"') + '"'
-    elif isinstance(mi_obj, (int, float)):
-        return str(mi_obj)
-    elif isinstance(mi_obj, bool):
-        return 'true' if mi_obj else 'false'
-    elif mi_obj is None:
-        return 'null'
-    elif isinstance(mi_obj, dict):
-        elementos = []
-        for clave, valor in mi_obj.items():
-            elementos.append(a_cadena_json(clave) + ':' + a_cadena_json(valor))
-        return '{' + ','.join(elementos) + '}'
-    elif isinstance(mi_obj, list):
-        elementos = [a_cadena_json(elemento) for elemento in mi_obj]
-        return '[' + ','.join(elementos) + ']'
-    else:
-        raise TypeError("El objeto de tipo {} no es serializable a JSON".format(type(mi_obj).__name__))
+    """
+    Converts an object (dictionary or list) into its JSON representation as a string.
+
+    Parameters:
+    my_obj (object): The object to be converted to JSON.
+
+    Returns:
+    str: The JSON representation of the object as a string.
+    """
+    return json.dumps(my_obj)
