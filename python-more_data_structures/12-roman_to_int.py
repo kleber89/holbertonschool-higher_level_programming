d8730a92f7eb457b48e0f0a7436a2e163777263c
kleber89 <benkosisackson@gmail.com>
2024-07-07 18:51:11 -0500

    update roman

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,20 +1,34 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or not roman_string:
-        return 0
 
-    roman_numerals = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+def roman_to_int(roman_str):
+    roman_to_decimal = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    roman_str = roman_str.upper()
+    n = len(roman_str)
+
+    decimal_values = []
+
+    for char in roman_str:
+        if char in roman_to_decimal:
+            decimal_values.append(roman_to_decimal[char])
+        else:
+            raise ValueError(f"invalid: {char}")
 
     total = 0
-    prev_value = 0
+    for i in range(n - 1):
+        if decimal_values[i] < decimal_values[i + 1]:
+            total -= decimal_values[i]
+        else:
+            total += decimal_values[i]
 
-    for char in roman_string:
-        current_value = roman_numerals.get(char, 0)
-        total += current_value
-
-        if current_value > prev_value:
-            total -= 2 * prev_value
-
-        prev_value = current_value
+    total += decimal_values[-1]
 
     return total
