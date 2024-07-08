e9ddbfe170e232adbeddabced58e4df49f89a96b
kleber89 <benkosisackson@gmail.com>
2024-07-08 17:49:05 -0500

    changes pascal code

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -4,34 +4,17 @@
 
 def pascal_triangle(n):
     """
-    Returns a list of lists of integers representing the Pascal’s triangle of n.
-
-    Args:
-        n (int): The number of rows in Pascal's Triangle.
-
-    Returns:
-        list: A list of lists of integers representing Pascal's Triangle.
+    returns a list of lists of integers representing the Pascal's triangle
     """
-    # Handle the case where n is less than or equal to 0
     if n <= 0:
         return []
-
-    # Initialize the Pascal's Triangle with the first row
     triangle = [[1]]
-
-    # Generate each row of the triangle
     for i in range(1, n):
-        # Start the new row with 1
         row = [1]
-
-        # Compute the inner elements of the row
         for j in range(1, i):
+            """Append the sum of the two elements."""
             row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-
-        # End the row with 1
         row.append(1)
-
-        # Append the new row to the triangle
         triangle.append(row)
 
     return triangle
