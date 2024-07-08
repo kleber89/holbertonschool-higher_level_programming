1e575e463cb7769259e13cd129d9ec126443c635
kleber89 <benkosisackson@gmail.com>
2024-07-08 17:23:16 -0500

    load changes class

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,30 +1,10 @@
 #!/usr/bin/python3
 """
-class_to_json
+function that returns the dictionary description with simple
+data structure (list, dictionary, string, integer and boolean).
 """
 
 
 def class_to_json(obj):
-    """
-    Returns the dictionary description with simple data structure
-    (list, dictionary, string, integer and boolean) for JSON serialization of an object.
-
-    Args:
-        obj: An instance of a class.
-
-    Returns:
-        A dictionary containing all serializable attributes of the object.
-    """
-    # Initialize an empty dictionary to store the attributes
-    obj_dict = {}
-
-    # Iterate over all attributes of the object
-    for key in dir(obj):
-        # Ignore private and special attributes
-        if not key.startswith("__"):
-            value = getattr(obj, key)
-            # Check if the attribute is serializable
-            if isinstance(value, (list, dict, str, int, bool)):
-                obj_dict[key] = value
-
-    return obj_dict
+    """ Return dic using dict"""
+    return obj.__dict__
