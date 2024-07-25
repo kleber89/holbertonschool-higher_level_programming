6265ead9bcc2ac173f2e7a86e02e8f26dac1606a
kleber89 <benkosisackson@gmail.com>
2024-07-25 17:05:06 -0500

    update code and style filter

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,28 +1,39 @@
 #!/usr/bin/python3
 """
-lists all cities from the database hbtn_0e_4_usa
+Displays all cities of a given state from the
+states table of the database hbtn_0e_4_usa.
+Safe from SQL injections.
 """
+
+
 from sys import argv
 import MySQLdb
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(
-            host='localhost',
-            port=3306,
-            user=argv[1],
-            passwd=argv[2],
-            db=argv[3],
-            charset='utf8')
+
+if __name__ == '__main__':
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    database_name = argv[3]
+    state_name = argv[4]
+
+    db = MySQLdb.connect(host='localhost', user=mysql_username,
+                         passwd=mysql_password, db=database_name,
+                         port=3306)
+
     cur = db.cursor()
 
-    sql = "SELECT cities.name\
-            FROM states\
-            INNER JOIN cities ON states.id  = cities.state_id\
-            WHERE states.name = %s\
-            ORDER BY cities.id ASC"
+    cur.execute(
+        "SELECT cities.name "
+        "FROM cities "
+        "INNER JOIN states ON cities.state_id = states.id "
+        "WHERE states.name = %s "
+        "ORDER BY cities.id",
+        (state_name,))
 
-    cur.execute(sql, (argv[4],))
-    print(", ".join([city[0] for city in cur.fetchall()]))
+    rows = cur.fetchall()
+
+    cities_list = ', '.join(row[0] for row in rows)
+    print(cities_list)
 
     cur.close()
     db.close()
